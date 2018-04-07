b16cad3ce45af12d1f28cb96058e9a9a46aae975
cdk1220 <donkuruppu@Dons-MacBook-Pro.local>
2018-04-07 16:58:58 -0500

    Created the needed number of vertices, and found out they were created at the origin using Gizmos.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Grid : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Sizd of the mesh to be generated
+    public int xSize, ySize;
+
+    private Vector3[] vertices;
+
+    private void Awake() {
+
+        // Generate as soon as empty game object awakens
+        Generate();
+    }
+
+    // This will generate the mesh
+    private void Generate()
+    {
+        // There is always one more vertex than tiles in each dimension
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+    }
+
+    // Called by Unity automatically
+    private void OnDrawGizmos() {
+
+        // Also invoked in the editor mode when vertices are not there, simply return
+        if (vertices == null) {
+            return;
+        }
+
+        Gizmos.color = Color.black;
+
+        for (int i = 0; i < vertices.Length; i++) {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
 }
